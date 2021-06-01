3447687b841b02ef7d04be04e855834a55c27932
Jared Meadows <meadowsjared@gmail.com>
2021-06-01 14:08:52 -0700

    got video demonstration working, set it to generate 1, 5, 10, 60, 120 fps of each site, restricted it to only do 500 frames, for testing, added mp4 files to gitignore

--- createVideos.py
+++ createVideos.py
@@ -3,7 +3,7 @@ from PIL import Image
 
 # filepaths
 fp_in = "*.png"
-fp_out = ".np4"
+fp_out = ".mp4"
 
 out_dir = "output"
 video_dir = "videos/"
@@ -15,39 +15,34 @@ def chunk_using_generators(image_ar, n):
 		yield image_ar[i:i + n]
 
 #creates a mp4 file, given an array of image file paths to add
-def create_video(image_ar, width, height, vid_filename):
+def create_video(image_ar, width, height, vid_filename, fp_out):
 	print('adding images... ', len(image_ar), 'frames at ('+str(width)+'x'+str(height)+')')
 	item_start_time = time.time()
 
 	# choose codec according to format needed
-	# fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-	fourcc = cv2.VideoWriter_fourcc('F','M','P','4')
-	video = cv2.VideoWriter(vid_filename, fourcc, 20.0, (width,height))
+	# you can set fourcc = -1 used to find the supported codecs on your system
+	for fps in [1, 5, 10, 20, 60, 120]:
+		fourcc = cv2.VideoWriter_fourcc(*'mp4v') #mp4
+		video = cv2.VideoWriter(vid_filename + '_' + str(fps) + 'fps' + fp_out, fourcc, fps, (width, height))
+		num_frames = 0
 
-	num_frames = 0
-	for file_path in image_ar:
-		num_frames += 1
-		img = cv2.imread(file_path)
-		print(file_path)
-		video.write(img)
-		if (num_frames > 5):
-			break
+		for file_path in image_ar:
+			num_frames += 1
+			if (num_frames % 20 == 0):
+				print(fps, num_frames)
 
-	video.release()
-	cv2.destroyAllWindows()
+			video.write(cv2.imread(file_path))
+			if (num_frames > 500):
+				break
 
-	file_exists = os.path.isfile(vid_filename)
-	if (file_exists):
-		print('file_exists = true')
-	else:
-		print('file_exists = false')
+		video.release()
+		cv2.destroyAllWindows()
 
 	chunk_time = time.time() - item_start_time
 	if (chunk_time > 60):
 		print(len(image_ar),'frames saved to', vid_filename, 'in', round((chunk_time)/60, 2), 'minutes')
 	else:
 		print(len(image_ar),'frames saved to', vid_filename, 'in', round(chunk_time), 'seconds')
-
 
 #used to get total run time at the end
 main_start_time = time.time()
@@ -71,7 +66,8 @@ for item in directory_contents:
 				im = Image.open(image_ar[0])
 				width, height = im.size
 				im.close()
-				create_video(image_ar, width, height, filename+'_'+str(len(image_ar))+fp_out)
+				create_video(image_ar, width, height, filename+'_'+str(len(image_ar)),fp_out)
+				break
 
 			print(site_name, 'processed in ', round(time.time() - location_start_time), 'seconds\n')
 
