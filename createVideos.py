97966b89f4d587315a1b24d063e80c169f25833c
Jared Meadows <meadowsjared@gmail.com>
2021-06-01 17:51:43 -0700

    passed in site_name, so I can log it with the progress

--- createVideos.py
+++ createVideos.py
@@ -15,7 +15,7 @@ def chunk_using_generators(image_ar, n):
 		yield image_ar[i:i + n]
 
 #creates a mp4 file, given an array of image file paths to add
-def create_video(image_ar, width, height, vid_filename, fp_out):
+def create_video(image_ar, width, height, site_name, vid_filename, fp_out):
 	print('adding images... ', len(image_ar), 'frames at ('+str(width)+'x'+str(height)+')')
 	item_start_time = time.time()
 
@@ -23,7 +23,7 @@ def create_video(image_ar, width, height, vid_filename, fp_out):
 	# you can set fourcc = -1 used to find the supported codecs on your system
 	threads = []
 	for fps in [1, 5, 10, 20, 60, 120]:
-		t = threading.Thread(target=create_video_fps, args=(vid_filename, fps, fp_out, width, height))
+		t = threading.Thread(target=create_video_fps, args=(site_name, vid_filename, fps, fp_out, width, height))
 		threads.append(t)
 		t.start()
 
@@ -37,7 +37,7 @@ def create_video(image_ar, width, height, vid_filename, fp_out):
 	else:
 		print(len(image_ar),'frames saved to', vid_filename, 'in', round(chunk_time), 'seconds')
 
-def create_video_fps(vid_filename, fps, fp_out, width, height):
+def create_video_fps(site_name, vid_filename, fps, fp_out, width, height):
 	fourcc = cv2.VideoWriter_fourcc(*'mp4v') #mp4
 	video = cv2.VideoWriter(vid_filename+'_'+str(fps)+'fps'+fp_out, fourcc, fps, (width, height))
 	num_frames = 0
@@ -45,7 +45,7 @@ def create_video_fps(vid_filename, fps, fp_out, width, height):
 	for file_path in image_ar:
 		num_frames += 1
 		if (num_frames % 20 == 0):
-			print(fps, 'fps, frame', num_frames)
+			print(site_name+':', fps, 'fps, frame', num_frames)
 		video.write(cv2.imread(file_path))
 
 	video.release()
@@ -62,21 +62,22 @@ directory_contents = glob.glob(out_dir+"/*/") # os.listdir(out_dir)
 for item in directory_contents:
 	if os.path.isdir(item):
 		location_start_time = time.time()
-		filename = video_dir+re.sub("/$", "", re.sub("^output/", "", item)) #name it the same as the directory
+		site_name = re.sub("/$", "", re.sub("^output/", "", item))
+		filename = video_dir+site_name #name it the same as the directory
 		if os.path.exists(filename):
 			# delete the old file to make room for it
 			os.remove(filename)
 		image_ar = sorted(glob.glob(item+fp_in))
-		site_name = re.sub("/$", "", re.sub("^output/", "", item))
 		print('processing '+site_name+' with', len(image_ar), 'total frames')
 
 		if len(image_ar) > 0:
 			im = Image.open(image_ar[0])
 			width, height = im.size
 			im.close()
-			create_video(image_ar, width, height, filename+'_'+str(len(image_ar)),fp_out)
+			create_video(image_ar, width, height, site_name, filename+'_'+str(len(image_ar)),fp_out)
 
 		print(site_name, 'processed in ', round(time.time() - location_start_time), 'seconds\n')
+		break
 
 total_time = round(time.time() - main_start_time)
 if (total_time > 60):
